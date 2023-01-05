3216fb2137be86e05b98ec3e1b5682e9eb0ee684
Bernd <bernd.furnier@fu-net.de>
2023-01-05 18:14:45 +0100

    initial VSC PlatformIO arduino blink

---
+++ lib/terminal/src/termout.cpp
@@ -0,0 +1,6 @@
+#include <termout.h>
+
+void serialDumpTime(void)
+{
+     Serial.print(", appending value ");   
+}
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+// #include <termout.h>
+#define BLINKLED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  // Serial monitor test
+  Serial.begin(9600);
+  Serial.println("Start");
+  
+  // set PIN 13 of blinking LED as OUTPUT
+  pinMode(BLINKLED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(BLINKLED, LOW);
+  // Serial.println("The blinkin LED is off");
+  delay(2500);
+  digitalWrite(BLINKLED, HIGH);
+  // Serial.println("The blinkin LED is on");
+  delay(1000);
+  /* Just checking an own lib 'termout.cpp'
+  serialDumpTime();
+  */
+ /* test of arduino function 'millis()'
+    millis() counts milli seconds since start of program
+ Serial.print("ms since start: ");
+ Serial.println(millis());
+ */
+}
