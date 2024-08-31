22c90b0f8a655f786ef06144335d373f8c75d2b7
Bernd <bernd.furnier@fu-net.de>
2024-08-31 18:52:29 +0200

    LED OFF: 1000ms, LED ON: 5000ms

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-// #include <termout.h>
+#include <termout.h>
 #define BLINKLED 13
 
 void setup() {
@@ -16,16 +16,16 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(BLINKLED, LOW);
   // Serial.println("The blinkin LED is off");
-  delay(2000);
+  delay(1000);
   digitalWrite(BLINKLED, HIGH);
   // Serial.println("The blinkin LED is on");
-  delay(4000);
+  delay(5000);
   /* Just checking an own lib 'termout.cpp'
   serialDumpTime();
   */
- /* test of arduino function 'millis()'
-    millis() counts milli seconds since start of program
- Serial.print("ms since start: ");
- Serial.println(millis());
- */
+  /* test of arduino function 'millis()'
+     millis() counts milli seconds since start of program
+  Serial.print("ms since start: ");
+  Serial.println(millis());
+  */
 }
